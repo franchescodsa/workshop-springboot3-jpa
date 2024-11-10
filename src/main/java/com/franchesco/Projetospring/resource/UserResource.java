0767a799a557e922590edce07a008d4381a5f076
franchesco aprigio <franchescodsa@outlook.com>
2024-11-09 23:38:31 -0300

    Service layer, component registration

--- src/main/java/com/franchesco/Projetospring/resource/UserResource.java
+++ src/main/java/com/franchesco/Projetospring/resource/UserResource.java
@@ -1,21 +1,35 @@
 package com.franchesco.Projetospring.resource;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.franchesco.Projetospring.entites.User;
+import com.franchesco.Projetospring.services.UserServices;
 
 @RestController // Indica que esta classe é um controlador REST, tornando possível expor métodos como endpoints de API
 @RequestMapping(value = "/users")//Define que todos os endpoints desta classe serão acessados com o caminho base /users
 public class UserResource {
 
+	
+	@Autowired
+    private UserServices service;
+	
 	@GetMapping //Indica que o método findAll será chamado em uma solicitação HTTP GET.
-	public ResponseEntity<User> findAll() {
-
-		User u = new User(1L, "Maria", "maria@gmail.com", "99999999", "12345");
-		return ResponseEntity.ok().body(u);
-
+	public ResponseEntity<List<User>> findAll() {
+        List<User> u = service.findAll();
+        return ResponseEntity.ok().body(u);
+    }
+	
+	@GetMapping(value=  "/{id}")
+	public ResponseEntity<User> findById(@PathVariable Long id) {
+       User obj = service.finById(id);
+        return ResponseEntity.ok().body(obj);
+    }
 	}
-}
+
---
+++ src/main/java/com/franchesco/Projetospring/services/UserServices.java
@@ -0,0 +1,33 @@
+package com.franchesco.Projetospring.services;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.franchesco.Projetospring.entites.User;
+import com.franchesco.Projetospring.repositories.UserRepository;
+
+@Service /*A anotação @Service indica ao Spring que essa classe é um componente de serviço. 
+Isso permite que ela seja gerenciada pelo Spring e injetada em outros componentes onde necessário.*/
+public class UserServices {
+
+	@Autowired
+	/*Injeção de Dependência: Como você já fez, 
+	 * @Autowired é usado para que o Spring injete automaticamente 
+	 * uma instância de UserRepository na classe UserServices.*/
+	private UserRepository repository;
+	
+	public List<User> findAll(){
+		return repository.findAll();
+		
+	}
+	
+	public User finById(Long id) {
+		Optional<User> obj = repository.findById(id);
+		return obj.get();
+	}
+	
+	
+}
